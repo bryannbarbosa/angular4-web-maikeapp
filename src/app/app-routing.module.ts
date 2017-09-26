03d3974a1c11da2b325f14a31ad9d5dd5d1dd93c
bryannbarbosa <bryannbarbosa@gmail.com>
2017-09-26 13:38:19 -0300

    files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule }              from '@angular/core';
+import { RouterModule, Routes }  from '@angular/router';
+
+import { SigninComponent }   from './signin/signin.component';
+import { SignupComponent }   from './signup/signup.component';
+
+const appRoutes: Routes = [
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent  },
+  { path: '',   redirectTo: '/signin', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
